ed53632c33780168497ac333b684df0c2e40919c
loser10085 <2783629723@qq.com>
2025-02-22 19:06:40 +0800

    Maven集中管理公共Jar包

---
+++ demo-common/src/main/java/com/wb/Main.java
@@ -0,0 +1,9 @@
+package com.wb;
+
+//import com.wb.dto.AiQADTO;
+//public class Main {
+//    public static void main(String[] args) {
+//        //new AiQADTO();
+//        //JsonUtils.convertToJson()
+//    }
+//}
--- demo-common/src/main/java/com/wb/utils/JsonUtils.java
+++ demo-common/src/main/java/com/wb/utils/JsonUtils.java
@@ -9,11 +9,41 @@ public class JsonUtils {
 
     /**
      * 将JSON字符串转换为JsonNode对象，支持直接访问属性
+     *
      * @param jsonString 合法的JSON格式字符串
      * @return JsonNode对象，可通过.get("属性名")访问数据
      * @throws JsonProcessingException 当JSON格式非法时抛出异常
      */
     public static JsonNode parseToJsonNode(String jsonString) throws JsonProcessingException {
         return objectMapper.readTree(jsonString);
+    }
+
+    /**
+     * 将JSON字符串映射为对应的Java类对象
+     * @param jsonString
+     * @param clazz
+     * @return
+     * @param <T>
+     * @throws JsonProcessingException
+     */
+    public static <T> T parseToEntity(String jsonString, Class<T> clazz) throws JsonProcessingException {
+        // 示例代码片段
+        return objectMapper.readValue(jsonString, clazz);
+    }
+
+    /**
+     * 将Java类对象转换为Json字符串
+     *
+     * @param userInfo
+     * @param <T>
+     * @return
+     */
+    public static <T> String convertToJson(T userInfo) {
+        try {
+            // 将对象（包含嵌套）序列化为JSON字符串
+            return objectMapper.writeValueAsString(userInfo);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("对象转JSON失败", e);
+        }
     }
 }
